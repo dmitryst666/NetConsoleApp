9a626ee63ff625dc03177f3584c6fe914bb3c372
dmitryst666 <dmitryst666@gmail.com>
2018-05-16 13:24:44 +0400

    Output is JSON Only

--- NetConsoleApp/Program.cs
+++ NetConsoleApp/Program.cs
@@ -17,16 +17,21 @@ namespace NetConsoleApp
 {
     class Program
     {
-
+        private static readonly int port = 8088;
         private static readonly HttpListener Listener = new HttpListener();
 
         public static void Main()
         {
-            Listener.Prefixes.Add("http://192.168.1.168:8088/");
+            Listener.Prefixes.Add(String.Format("http://+:{0}/",port));
             Listener.Start();
             Listen();
             Console.WriteLine("Listening...");
+            string ipHost = System.Net.Dns.GetHostName();
+            IPAddress localAddr = System.Net.Dns.GetHostByName(ipHost).AddressList[0];
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("IP: {0}       Port: {1}", localAddr, port);
             Console.WriteLine("Press any key to exit...");
+            Console.ResetColor();
             Console.ReadKey();
         }
 
@@ -44,7 +49,9 @@ namespace NetConsoleApp
 
         private static void ProcessRequest(HttpListenerContext context)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Thread no {0}", Environment.CurrentManagedThreadId);
+            Console.ResetColor();
             HttpListenerRequest request = context.Request;
             //  string path = System.Net.WebUtility.UrlDecode(request.RawUrl);
             string text;
@@ -75,24 +82,16 @@ namespace NetConsoleApp
 
             
 
-            //ArrayList tt = new ArrayList();
-
-            //for (int i=0; i < 10; i++ )
-            //{
-
-            //    testObject obj = new testObject();
-            //    obj.name = "Test Object No. " + i;
-            //    obj.account = "141000000" + i;
-            //    tt.Add(obj);
-            //}
-
+  
 
             ArrayList rr = new ArrayList();
 
             for (int j = 0; j < 10; j++)
             {
                 ClientData cld = new ClientData();
-                cld.name = String.Format("Client No. {0}", j);
+                Random rnd = new Random();
+                
+                cld.name = String.Format("Client {0} No. {1}",rnd.Next(10,100) ,j);
                 cld.account = String.Format("141000000{0}", j);
                 cld.address = String.Format("Address: 1st street #{0}", j);
                 rr.Add(cld);
@@ -108,23 +107,25 @@ namespace NetConsoleApp
 
                     response.StatusCode = (int)HttpStatusCode.OK;
                 ////Content-Type: text/html; charset=UTF-8
-                response.AddHeader(HttpResponseHeader.ContentType.ToString(), "text/html; charset=utf-8");
-                string responseString = "<!DOCTYPE html><HTML lang=\"ru\"><HEAD><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>";
-                responseString += "<meta http-equiv=Content-Type content=\"text/html; charset = UTF-8\" /></HEAD><BODY>";
-                responseString += "<div id=\"accz\"></div> <div id=\"namez\"></div> <div id=\"addrz\"></div>";
-                responseString += "Input form <br />";
-                
-                responseString += "<form action=\"\\\" method=\"POST\" name=\"test\">";
-                responseString += "<input type=\"text\" name=\"textparam\" value=\"\" />";
-                responseString += "<input type=\"submit\" value=\"Send\">";
-                responseString += "</form>";
-             //   responseString += json;
-                responseString += "<script>";
-                responseString += @"var obj = jQuery.parseJSON( '" + json + "' ); jQuery.each( obj, function( i, val ) {" +
-                    "  $(\"#accz\").append(\"<br /> - \" + val[\"account\"]);  $(\"#namez\").append(\"<br /> - \" + val[\"name\"]);   $(\"#addrz\").append(\"<br /> - \" + val[\"address\"]); }); ";
-                ///  $("#accz").append("<br /><b>AAAAAA</b>");
-                responseString += "</script>";
-                responseString +="</BODY></HTML>";
+               // response.AddHeader(HttpResponseHeader.ContentType.ToString(), "text/html; charset=utf-8");
+                response.AddHeader(HttpResponseHeader.ContentType.ToString(), "application/json; charset=utf-8");
+                string responseString = "";
+                //responseString = "<!DOCTYPE html><HTML lang=\"ru\"><HEAD><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>";
+                //responseString += "<meta http-equiv=Content-Type content=\"text/html; charset = UTF-8\" /></HEAD><BODY>";
+                //responseString += "<div id=\"accz\"></div> <div id=\"namez\"></div> <div id=\"addrz\"></div>";
+                //responseString += "Input form <br />";
+
+                //responseString += "<form action=\"\\\" method=\"POST\" name=\"test\">";
+                //responseString += "<input type=\"text\" name=\"textparam\" value=\"\" />";
+                //responseString += "<input type=\"submit\" value=\"Send\">";
+                //responseString += "</form>";
+                responseString += json;
+                //responseString += "<script>";
+                //responseString += @"var obj = jQuery.parseJSON( '" + json + "' ); jQuery.each( obj, function( i, val ) {" +
+                //    "  $(\"#accz\").append(\"<br /> - \" + val[\"account\"]);  $(\"#namez\").append(\"<br /> - \" + val[\"name\"]);   $(\"#addrz\").append(\"<br /> - \" + val[\"address\"]); }); ";
+                /////  $("#accz").append("<br /><b>AAAAAA</b>");
+                //responseString += "</script>";
+                //responseString +="</BODY></HTML>";
                     byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
                     // Get a response stream and write the response to it.
                     response.ContentLength64 = buffer.Length;
